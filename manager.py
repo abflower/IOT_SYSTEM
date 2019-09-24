e4d628844d8a0d4aab2ae43099a6d1401d3ffe08
abellafiore <alessandro.bellafiore@zynstra.com>
2019-09-24 16:24:50 +0100

    minor changes

--- manager.py
+++ manager.py
@@ -151,6 +151,41 @@ def create_app():
         print('click restart')
         restart()
         return redirect('/')
+
+    @app.route('/autom_list')
+    def autom_list():
+        automations_list = []
+        for automation in automations:
+            field = {}
+            field['id'] = automation.id
+            field['name'] = automation.name
+            field['status'] = automation.status
+            automations_list.append(field)
+        response = make_response(render_template("automations_list.html", automations_list=automations_list))
+        return response
+
+    @app.route('/autom_page')
+    def autom_page():
+        id = request.args.get('id')
+        response=''
+        for automation in automations:
+            if automation.id == id:
+                info = automation.return_all_attrs()
+                service_fields = []
+                for param in automation.extra_params:
+                    field = {}
+                    field['name'] = param['name']
+                    field['value'] = automation.params[param['name']]
+                    service_fields.append(field)
+                conditions_list = []
+                for key in automation.conditions.keys():
+                    field = {}
+                    field['name'] = key
+                    field['value'] = automation.conditions[key]
+                    conditions_list.append(field)
+
+                response = make_response(render_template("single_automation.html", info=info, service_fields=service_fields, conditions_list=conditions_list))
+        return response    
  
 
     return app
